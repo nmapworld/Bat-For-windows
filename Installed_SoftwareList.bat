739994a72b31cc4137674e399d12e5f2e15d0c08
nmapworld <1375000358@qq.com>
2021-06-10 01:04:17 +0800

    1

--- Installed_SoftwareList.bat
+++ Installed_SoftwareList.bat
@@ -1,5 +1,9 @@
 set filename=software.txt
+::Reg Read
 echo 64Bits:>%userprofile%\desktop\%filename%
 reg query HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall /f DisplayName /t REG_SZ /s |findstr REG_SZ>>%userprofile%\desktop\%filename%
 echo 32Bits:>>%userprofile%\desktop\%filename%
 reg query HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall /f DisplayName /t REG_SZ /s |findstr REG_SZ>>%userprofile%\desktop\%filename%
+
+::Powershell
+::PowerShell -Command "& {Get-StartApps |format-list}">>%userprofile%\desktop\%filename%
