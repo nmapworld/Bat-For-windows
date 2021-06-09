9027e53aea22f70660ea10509925d0dac3e36ff7
nmapworld <1375000358@qq.com>
2021-06-10 02:36:20 +0800

    1

--- Firewall_Clear.bat
+++ Firewall_Clear.bat
@@ -1,5 +1,5 @@
 netsh advfirewall set allprofiles state on
-netsh  advfirewall firewall delete rule  name=all
+netsh advfirewall firewall delete rule  name=all
 netsh advfirewall set allprofiles firewallpolicy blockinbound,allowoutbound
 netsh advfirewall firewall add rule  name="tcp" dir=in protocol=tcp localport=3389 action=allow
 netsh advfirewall firewall add rule  name="udp" dir=in protocol=udp localport=3389 action=allow
--- Uninstall_DefaultAPP_Win10_full.bat
+++ Uninstall_DefaultAPP_Win10_full.bat
@@ -1,9 +1,9 @@
 @echo off
 
-@REM keep Microsoft.WindowsStore && Microsoft.MicrosoftEdge
-set list=Microsoft.549981C3F5F10 Microsoft.WindowsAlarms Microsoft.MicrosoftStickyNotes Microsoft.SkypeApp Microsoft.WindowsFeedbackHub Microsoft.MicrosoftOfficeHub Microsoft.Office.OneNote Microsoft.Microsoft3DViewer Microsoft.XboxGameCallableUI Microsoft.XboxGameOverlay Microsoft.Xbox.TCUI  Microsoft.XboxApp Microsoft.XboxIdentityProvider Microsoft.XboxSpeechToTextOverlay Microsoft.XboxGamingOverlay Microsoft.GamingServices microsoft.windowscommunicationsapps Microsoft.BingWeather Microsoft.ZuneVideo Microsoft.ZuneMusic Microsoft.People Microsoft.Windows.PeopleExperienceHost Microsoft.YourPhone Microsoft.MicrosoftSolitaireCollection Microsoft.GetHelp  Microsoft.WindowsMaps Microsoft.WindowsSoundRecorder
+@REM keep WindowsStore && MicrosoftEdge
+set list=549981C3F5F10 WindowsAlarms MicrosoftStickyNotes SkypeApp WindowsFeedbackHub MicrosoftOfficeHub Office.OneNote Microsoft3DViewer XboxGameOverlay Xbox.TCUI  XboxApp XboxIdentityProvider XboxSpeechToTextOverlay XboxGamingOverlay GamingServices windowscommunicationsapps BingWeather ZuneVideo ZuneMusic People Windows.PeopleExperienceHost YourPhone MicrosoftSolitaireCollection GetHelp  WindowsMaps WindowsSoundRecorder
 
 for %%a in (%list%) do (
     echo %%a
-    PowerShell -Command "& {Get-AppxPackage -alluser %%a | Remove-AppxPackage}"
+    PowerShell -Command "& {Get-AppxPackage -alluser Microsoft.%%a | Remove-AppxPackage}"
 )
