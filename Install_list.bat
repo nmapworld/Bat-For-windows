4a47aa65e30cd574bc2417127daeabbc66be91f6
nmapworld <1375000358@qq.com>
2021-06-10 02:58:28 +0800

    1

---
+++ Install_list.bat
@@ -0,0 +1,12 @@
+@REM chrome
+start https://www.google.cn/chrome/
+@REM 7z
+start https://sparanoid.com/lab/7z/download.html
+@REM Sublime
+start https://www.sublimetext.com/3
+@REM Android Vritual
+start https://www.memuplay.com/
+@REM QQ
+start https://im.qq.com/pcqq/
+@REM Wechat
+start https://windows.weixin.qq.com/
