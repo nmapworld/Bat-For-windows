ab176b805c2640387fc08ccae506f63b5aa56784
nmapworld <1375000358@qq.com>
2021-06-10 20:35:52 +0800

    fix Chinese lanuage System

--- 9.WifiPassword_clear.bat
+++ 9.WifiPassword_clear.bat
@@ -3,4 +3,5 @@ echo "Will Creat：%userprofile%/desktop/wifi_password.txt"
 pause
 netsh wlan show profiles |findstr :> %tmp%\wifi1.log
 echo >%userprofile%/desktop/wifi_password.txt
-for /f "delims=: tokens=2" %%i in (%tmp%\wifi1.log) do netsh wlan show profiles name=%%i key=clear| findstr /i  /C:"SSID name" /C:"Key Content" /C:"---------------------"  1>>%userprofile%/desktop/wifi_password.txt
+:: for /f "delims=: tokens=2" %%i in (%tmp%\wifi1.log) do netsh wlan show profiles name=%%i key=clear| findstr /i  /C:"SSID name" /C:"Key Content" /C:"---------------------">>%userprofile%/desktop/wifi_password.txt
+for /f "delims=: tokens=2" %%i in (%tmp%\wifi1.log) do netsh wlan show profiles name=%%i key=clear>>%userprofile%/desktop/wifi_password.txt
