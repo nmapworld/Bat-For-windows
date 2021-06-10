d002fb63785d550b6b531f928574ad4b5af0095c
nmapworld <1375000358@qq.com>
2021-06-10 19:19:32 +0800

    Update 9.WifiPassword_clear.bat

--- 9.WifiPassword_clear.bat
+++ 9.WifiPassword_clear.bat
@@ -1,3 +1,6 @@
+@echo off
+echo "Will Creat：%userprofile%/desktop/wifi_password.txt"
+pause
 netsh wlan show profiles |findstr Profile> %tmp%\wifi1.log
 echo >%userprofile%/desktop/wifi_password.txt
 for /f "delims=: tokens=2" %%i in (%tmp%\wifi1.log) do netsh wlan show profiles name=%%i key=clear| findstr /i  /C:"SSID name" /C:"Key Content" /C:"---------------------"  1>>%userprofile%/desktop/wifi_password.txt
