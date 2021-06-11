6296f425df78644f11b32632295a1206fe7d1575
nmapworld <1375000358@qq.com>
2021-06-11 09:13:53 +0800

    2

--- 1.Defender_Disable.bat
+++ 1.Defender_Disable.bat
@@ -1,7 +1,19 @@
 @echo off
 echo "will Disable Defender"
 pause
-rem Stop Disable Defender
-reg add HKLM\SOFTWARE\Microsoft\Windows" "Defender /v DisableAntiVirus /t REG_DWORD /d 1 /f
-rem Open Disable Defender
-::reg add HKLM\SOFTWARE\Microsoft\Windows" "Defender /v DisableAntiVirus /t REG_DWORD /d 0 /f
+rem Disable Defender
+REG ADD HKLM\SOFTWARE\Microsoft\Windows" "Defender /v DisableAntiVirus /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Microsoft\Windows" "Defender /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows" "Defender /v DisableAntiVirus /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\DeviceHealthAttestationService /v EnableDeviceHealthAttestationService /t REG_DWORD /d  0 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows" "Defender /v ServiceKeepAlive /t REG_DWORD /d  0 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows" "Defender /v DisableRoutinelyTakingAction /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows" "Defender /v DisableAntiSpyware /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows" "Defender /v DisableAntiVirus /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows" "Defender\Scan /v DisableReparsePointScanning /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows" "Defender\Real-Time" "Protection /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows" "Defender\Real-Time" "Protection /v DisableBehaviorMonitoring /t REG_DWORD /d 1 /f
+REG ADD HKLM\SOFTWARE\Policies\Microsoft\Windows" "Defender\Real-Time" "Protection /v DisableIOAVProtection /t REG_DWORD /d 1 /f
+REG ADD HKLM\System\CurrentControlSet\Control\Storage\EnabledDenyGP /v DenyAllGPState /t REG_DWORD /d 1 /f
+REG ADD HKEY_LOCAL_MACHINE\System\currentcontrolset\services\SecurityHealthService /v start /t REG_DWORD /d 4 /f
+REG DELETE HKEY_LOCAL_MACHINE\software\microsoft\windows\currentversion\run /v SecurityHealth /f
--- 2.Uninstall_DefaultAPP_Win10_full.bat
+++ 2.Uninstall_DefaultAPP_Win10_full.bat
@@ -2,7 +2,7 @@
 echo "Will Uninstall Default APP in Windows10"
 pause
 @REM keep WindowsStore && MicrosoftEdge
-set list=549981C3F5F10 WindowsAlarms MicrosoftStickyNotes SkypeApp WindowsFeedbackHub MicrosoftOfficeHub Office.OneNote Microsoft3DViewer XboxGameOverlay Xbox.TCUI  XboxApp XboxIdentityProvider XboxSpeechToTextOverlay XboxGamingOverlay GamingServices windowscommunicationsapps BingWeather ZuneVideo ZuneMusic People Windows.PeopleExperienceHost YourPhone MicrosoftSolitaireCollection GetHelp  WindowsMaps WindowsSoundRecorder
+set list=549981C3F5F10 MixedReality.Portal Getstarted Windows.SecHealthUI WindowsAlarms MicrosoftStickyNotes SkypeApp WindowsFeedbackHub MicrosoftOfficeHub Office.OneNote Microsoft3DViewer XboxGameOverlay Xbox.TCUI  XboxApp XboxIdentityProvider XboxSpeechToTextOverlay XboxGamingOverlay GamingServices windowscommunicationsapps BingWeather ZuneVideo ZuneMusic People Windows.PeopleExperienceHost YourPhone MicrosoftSolitaireCollection GetHelp  WindowsMaps WindowsSoundRecorder
 
 for %%a in (%list%) do (
     echo %%a
