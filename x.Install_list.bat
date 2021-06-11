331848d13ca39a947f4a6cbcb9359cb5d04c177d
nmapworld <1375000358@qq.com>
2021-06-11 22:10:38 +0800

    Update x.Install_list.bat

--- x.Install_list.bat
+++ x.Install_list.bat
@@ -1,12 +1,20 @@
 @REM chrome
 start https://www.google.cn/chrome/
+@REM Firefox
+https://www.mozilla.org/en-US/
 @REM 7z
 start https://sparanoid.com/lab/7z/download.html
 @REM Sublime
 start https://www.sublimetext.com/3
+@REM Markdown edit
+start https://typora.io/
 @REM Android Vritual
 start https://www.memuplay.com/
 @REM QQ
 start https://im.qq.com/pcqq/
 @REM Wechat
 start https://windows.weixin.qq.com/
+@REM Video Player
+start https://potplayer.daum.net/
+@REM Proxy for socks/http
+start https://www.proxifier.com/
