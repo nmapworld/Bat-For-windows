0832559187b9ae3ca54cdd7992dbe6d2c2203ea1
Ping <ping@Macbook-Pro.local>
2022-02-21 10:50:42 +0800

    1
    1

--- 8.ChangePort_RDP.bat
+++ 8.ChangePort_RDP.bat
@@ -4,13 +4,13 @@ set PortNumber=3389
 title Change RDP_Port %PortNumber%
 pause
 @REM Start RDP
-REG add HKLM\SYSTEM\CurrentControlSet\Control\Terminal" "Server /v fDenyTSConnections /t REG_DWORD /d 0 /f
+REG add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
 @REM Stop RDP
 ::REG add HKLM\SYSTEM\CurrentControlSet\Control\Terminal" "Server /v fDenyTSConnections /t REG_DWORD /d 1 /f
 @REM Change TCPPort for RDP
-reg add HKLM\SYSTEM\CurrentControlSet\Control\Terminal" "Server\Wds\rdpwd\Tds\tcp /v PortNumber /t REG_DWORD /d %PortNumber% /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\Wds\rdpwd\Tds\tcp" /v PortNumber /t REG_DWORD /d %PortNumber% /f
 @REM Change UDPPort for RDP
-reg add HKLM\SYSTEM\CurrentControlSet\Control\Terminal" "Server\WinStations\RDP-Tcp /v PortNumber /t REG_DWORD /d %PortNumber% /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v PortNumber /t REG_DWORD /d %PortNumber% /f
 @REM check RDPport
 reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /V PortNumber
 @REM Permit RPD from Advfirewall
